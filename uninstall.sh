8fb0690c06c7225554f184fcb7f42720d4441822
CLQ060728 <55714890+CLQ060728@users.noreply.github.com>
2024-09-09 05:57:45 +0200

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,4 +0,0 @@
-pip uninstall qiskit-algorithms qiskit-ibmq-provider tensorflow torchaudio qiskit-optimization dwave-system dwavebinarycsp penaltymodel -y
-pip uninstall dwave-hybrid dwave-inspector dwave-ocean-sdk -y
-pip uninstall dwave-cloud-client dwave-greedy dwave-neal dwave-networkx dwave-preprocessing dwave-samplers dwave-tabu -y
-pip uninstall minorminer ortools -y
