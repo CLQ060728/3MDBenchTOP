9ea1d3a6c0c78afa667c6a29c64f682d370548e7
CLQ060728 <liu.qian.pro@gmail.com>
2024-10-20 20:43:14 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 pip install --upgrade -r requirements.txt
-git clone https://github.com/NVIDIA/apex.git ./ds_processors/video_generators/OpenSora1_2/
+git clone https://github.com/NVIDIA/apex.git ./ds_processors/video_generators/OpenSora1_2/apex/
+pip install --upgrade torch==2.4.0 --index-url https://download.pytorch.org/whl/cu124
 pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./ds_processors/video_generators/OpenSora1_2/apex/
