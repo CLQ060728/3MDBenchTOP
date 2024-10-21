76967ff0a06f9f70498a8da1e6250df5672ca6a8
CLQ060728 <liu.qian.pro@gmail.com>
2024-10-21 18:56:00 +0000

    updated install.sh with colossalai installation scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
+git clone https://github.com/hpcaitech/ColossalAI.git ./ds_processors/video_generators/OpenSora1_2/colossalai/
+rm -rf ./ds_processors/video_generators/OpenSora1_2/colossalai/.git
+rm -rf ./ds_processors/video_generators/OpenSora1_2/colossalai/.github
+rm -rf ./ds_processors/video_generators/OpenSora1_2/colossalai/requirements/
+cp -r ./ds_processors/video_generators/OpenSora1_2/requirements/ ./ds_processors/video_generators/OpenSora1_2/colossalai/
 pip install --upgrade -r requirements.txt
+pip install --upgrade ./ds_processors/video_generators/OpenSora1_2/colossalai/
 git clone https://github.com/NVIDIA/apex.git ./ds_processors/video_generators/OpenSora1_2/apex/
-pip install --upgrade torch==2.4.0 --index-url https://download.pytorch.org/whl/cu124
 pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./ds_processors/video_generators/OpenSora1_2/apex/
