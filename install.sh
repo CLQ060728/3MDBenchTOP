b7c77952b2c083eacb68fe6bf8e999fbdf39d0b1
Qian Liu <liu.qian.pro@gmail.com>
2024-11-12 17:23:31 +0100

    updated install.sh changed the order of apex and colossalai installation

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ pip install --upgrade setuptools==75.2.0
 pip install --upgrade wheel==0.44.0
 pip install --upgrade torch==2.1.2
 pip install --upgrade -r requirements.txt
-pip install --upgrade ./ds_processors/video_generators/OpenSora1_2/colossalai/
 pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./ds_processors/video_generators/OpenSora1_2/apex/
+pip install --upgrade ./ds_processors/video_generators/OpenSora1_2/colossalai/
 pip install --upgrade ./ds_processors/prompt_processors/PLLaVA/moviepy/
 wget "https://github.com/w86763777/pytorch-gan-metrics/releases/download/v0.1.0/pt_inception-2015-12-05-6726825d.pth" --directory-prefix=./ds_profiling/EvalCrafter/checkpoints/
 mkdir -p ./ds_profiling/EvalCrafter/checkpoints/DOVER/pretrained_weights/
